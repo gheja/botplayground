5e5846b588ccbc9c4dddd515ca0d4de246d29095
gheja <gheja@m002560p-ubuntu-2019>
2019-07-29 20:12:02 +0200

    Restructured index a bit

--- src/website/index.php
+++ src/website/index.php
@@ -3,16 +3,15 @@
 	
 	session_start();
 	
-	if (array_key_exists("user_name", $_SESSION))
-	{
-		echo "Hello " . $_SESSION["user_full_name"] . "!<br/>";
-		echo "<a href=\"logout\">Log out</a>";
-		
-		print_r($_SESSION);
-	}
-	else
+	if (!array_key_exists("user_name", $_SESSION))
 	{
 		echo "Welcome, please ";
 		echo "<a href=\"https://github.com/login/oauth/authorize?scope=user:read&client_id=" . GITHUB_CLIENT_ID . "\">log in with GitHub</a>.";
+		die();
 	}
+	
+	echo "Hello " . $_SESSION["user_full_name"] . "!<br/>";
+	echo "<a href=\"logout\">Log out</a>";
+	
+	print_r($_SESSION);
 ?>
