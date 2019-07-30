33fac1053d6913af6b9d8a4def53d3b5f30cc18d
gheja <gheja@m002560p-ubuntu-2019>
2019-07-30 19:56:18 +0200

    Added listing of currently registered bots to index

--- src/website/index.php
+++ src/website/index.php
@@ -10,8 +10,58 @@
 		die();
 	}
 	
-	echo "Hello " . $_SESSION["user_full_name"] . "!<br/>";
-	echo "<a href=\"logout\">Log out</a>";
+	$db = new PDO("mysql:host=" . DB_HOSTNAME . ";dbname=" . DB_DATABASE, DB_USERNAME, DB_PASSWORD);
 	
-	print_r($_SESSION);
+	if (!$db)
+	{
+		echo "Cannot reach database.";
+		die();
+	}
+	
+	// fetch all games
+	$statement = $db->prepare("SELECT * FROM game WHERE is_deleted != 1");
+	$statement->execute();
+	
+	$games = $statement->fetchAll(PDO::FETCH_ASSOC);
+	
+	
+	// fetch all bots owned by the user
+	$statement = $db->prepare("SELECT * FROM bot WHERE github_user_id = ? AND is_deleted != 1");
+	$statement->bindValue(1, $_SESSION["user_id"]);
+	$statement->execute();
+	
+	$bots = $statement->fetchAll(PDO::FETCH_ASSOC);
+	
+	
+	echo "Hello " . $_SESSION["user_full_name"] . "!<br/>";
+	echo "<br/>";
+	echo "You have <b>" . count($bots) . " bots</b> registered in total out of " . BOT_COUNT_LIMIT_PER_USER . ".<br/>";
+	echo "<br/>";
+	echo "<a href=\"logout\">Log out</a><br/>";
+	
+	echo "<h2>Games</h2>";
+	
+	// display all games and bots registered for those games
+	foreach ($games as $game)
+	{
+		echo "<h3>" . $game["game_title"] . "</h3>";
+		echo "<p>" . $game["game_description"] . "</p>";
+		echo "<ul>";
+		foreach ($bots as $bot)
+		{
+			if ($bot["game_id"] != $game["game_id"])
+			{
+				continue;
+			}
+			
+			echo "<li>";
+			echo "Client ID: " . $bot["bot_client_id"] . "<br/>";
+			echo "Secret: " . $bot["bot_secret"] . "<br/>";
+			echo "Current match ID: (none)<br/>";
+			echo "<a href=\"?action=delete_bot&bot_client_id=" . $bot["bot_client_id"] . "\" onclick=\"javascript: return confirm('Are you sure you want to delete this bot?');\">Delete</a>";
+			echo "</li>";
+		}
+		echo "<li><a href=\"?action=new_bot&game_id=" . $game["game_id"] . "\">Add new</a></li>";
+		echo "</ul>";
+	}
 ?>
