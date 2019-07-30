6023933464e468e0d510001857cd71dd552c0183
gheja <gheja@m002560p-ubuntu-2019>
2019-07-30 19:54:40 +0200

    Added test games to database

---
+++ src/database/0030_test_games.sql
@@ -0,0 +1,5 @@
+INSERT INTO `game` (`game_id`, `game_name`, `game_title`, `game_description`, `game_players`, `is_deleted`, `create_time`) VALUES
+(1, 'guess10', 'Guess 10', 'Guess a number between 1 to 10.', 1, 0, 0),
+(2, 'rockpaperscissors', 'Rock Paper Scissors', 'A two player Rock Paper Scissors, best out of 3 wins.', 2, 0, 0);
+
+COMMIT;
